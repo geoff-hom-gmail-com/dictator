d43d78bc98deb4f96639f9ad19d02b5de8662cc4
Geoffrey Hom <geoffhom@gmail.com>
2014-04-02 18:22:39 -0700

    Cleaned up code. Proper version number increase.

--- Dictator/GGKNightSummaryViewController.m
+++ Dictator/GGKNightSummaryViewController.m
@@ -11,7 +11,6 @@
 #import "GGKGameModel.h"
 
 @interface GGKNightSummaryViewController ()
-
 @end
 
 @implementation GGKNightSummaryViewController
@@ -56,25 +55,6 @@
     if (!someoneWasEliminated) {
         [theSummaryTextMutableString appendFormat:@"\n\nNo one was %@….", GGKEliminatedString];
     }
-    
-//    if ((thePlayersEliminatedLastNightArray == nil) || ([thePlayersEliminatedLastNightArray count] == 0)) {
-//        theSummaryTextMutableString = [NSMutableString stringWithFormat:@"Last night, no one was %@.", GGKEliminatedString];
-//    } else {
-//        // For now, assuming only 0 or 1 players can be eliminated.
-//        GGKPlayer *theEliminatedPlayer = thePlayersEliminatedLastNightArray[0];
-//        // If there was a voting tie, note that.
-//        NSString *theTieTextString = @"";
-//        if (self.gameModel.thereWasATieBOOL) {
-//            // "Last night, whispered arguments were heard … and %@ was eliminated!"
-//            theTieTextString = @"whispered arguments were heard … and ";
-//        }
-//        theSummaryTextMutableString = [NSString stringWithFormat:@"Reveal this to all:"
-//                                "\n\nLast night, %@%@ was %@!"
-//                                "\n%@ was %@.", theTieTextString, theEliminatedPlayer.name, GGKEliminatedString, theEliminatedPlayer.name, theEliminatedPlayer.role.longNameWithArticle];
-//    }
     self.summaryTextView.text = theSummaryTextMutableString;
-//    if ([self.gameModel isGameOver]) {
-//        NSLog(@"TEMP NOTE: Game should be over!");
-//    }
 }
 @end
